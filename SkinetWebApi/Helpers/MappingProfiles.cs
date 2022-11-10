01ef8c59f521e5c9b7f8b1c240ac62cf7f66ba20
BADAH Bilal <bilalbadah@gmail.com>
2022-11-10 17:09:58 +0100

    This commit for section 4 episode 045 Adding a Custom Value Resolver for AutoMapper ==> In this episode we solve to return the full url of picture that is mean domain + path of picture <==

--- SkinetWebApi/Helpers/MappingProfiles.cs
+++ SkinetWebApi/Helpers/MappingProfiles.cs
@@ -11,7 +11,7 @@ namespace SkinetWebApi.Helpers
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d=>d.ProductBrand, o => o.MapFrom(s=> s.ProductBrand.Name))
                 .ForMember(d=>d.ProductType, o => o.MapFrom(s=> s.ProductType.Name))
-                ;
+                .ForMember(d=> d.PictureUrl, o => o.MapFrom<ProductUrlResorver>());
 
         }
     }
---
+++ SkinetWebApi/Helpers/ProductUrlResorver.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+using SkinetWebApi.Dtos;
+
+namespace SkinetWebApi.Helpers
+{
+    public class ProductUrlResorver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ProductUrlResorver(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _config["ApiUrl"] + source.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}
